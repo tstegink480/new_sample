c3235bd08bd39a1b10c6888f3ab77a9468d3846b
Trent Stegink <trent_stegink@taylor.edu>
2012-11-12 19:50:19 -0500

    Done with it all, including chapter 10 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+    
+  
     private
     
     def correct_user
@@ -67,4 +82,5 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user.admin?
     end
     
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -64,6 +64,19 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+      
+    describe "in the Relationships controller" do
+      
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { response.should redirect_to(signin_path)}
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { response.should redirect_to(signin_path) }
+      end
+    end
 
         describe "after signing in" do
 
@@ -102,6 +115,16 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+        
+          describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Following') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Followers') }
         end
       end
     end
